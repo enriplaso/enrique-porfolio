186116cde6647f92ba969faf719fc11945447ff5
Enrique Plaza <enriplaso@gmail.com>
2025-09-26 11:38:56 +0200

    nave menu

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -94,8 +94,8 @@ export const Background = () => {
           key={meteor.id}
           className="meteor animate-meteor"
           style={{
-            width: meteor.size * 50 + "px",
-            height: meteor.size * 2 + "px",
+            width: meteor.size * 40 + "px",
+            height: meteor.size + "px",
             left: meteor.x + "%",
             top: meteor.y + "%",
             // animationDelay: meteor.delay,
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import { ThemeToggle } from "../components/ThemeToggle";
+import { Navbar } from "@/components/NavBar";
+import { ThemeToggle } from "@/components/ThemeToggle";
 import { Background } from "@/components/Background";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle />
       <Background />
+      <Navbar />
     </div>
   );
 };
