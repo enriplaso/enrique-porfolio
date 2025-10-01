ac8f1a479df5da1c916cee82ce09d66be759d7ec
Enrique Plaza <enriplaso@gmail.com>
2025-10-01 10:46:23 +0200

    HeroSection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,18 @@
 import { Navbar } from "@/components/NavBar";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { Background } from "@/components/Background";
+import { HeroSection } from "@/components/HeroSection";
+
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle />
       <Background />
       <Navbar />
+
+      <main>
+        <HeroSection></HeroSection>
+      </main>
     </div>
   );
 };
